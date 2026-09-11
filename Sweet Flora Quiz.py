d386cf03bb3cb7742839195a2932708a91a3ea23
406-44-manutsawin <56823@yupparaj.ac.th>
2026-09-11 09:55:03 +0700

    Update Sweet Flora Quiz.py

--- Sweet Flora Quiz.py
+++ Sweet Flora Quiz.py
@@ -133,7 +133,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans
         st.success("🎉 You win!")
     else:
         score < 7:
-        st.success("Improve your skill")
+        st.error("Improve your skill")
 
     if score < 3:
         st.success("You lose")
