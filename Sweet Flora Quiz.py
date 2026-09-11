fe7a00c660fc57a93075a18f0b710099d0f01e1c
406-44-manutsawin <56823@yupparaj.ac.th>
2026-09-11 10:36:42 +0700

    Update Sweet Flora Quiz.py

--- Sweet Flora Quiz.py
+++ Sweet Flora Quiz.py
@@ -129,7 +129,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
 
     
-st.info("💗 ได้คะแนนรวม: {} คะแนน".format(score))
+st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
 if score == 10:
     st.success("🎉 You win!")
