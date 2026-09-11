bb907ffdb2f6370bc5c6a66a362384afe33c9c4a
406-44-manutsawin <56823@yupparaj.ac.th>
2026-09-11 10:35:51 +0700

    Refactor score display to use format method

--- Sweet Flora Quiz.py
+++ Sweet Flora Quiz.py
@@ -129,7 +129,7 @@ def show_result_dialog(ans1, ans2, ans3, ans4, ans5, ans6, ans7, ans8, ans9, ans
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
 
     
-st.info(f"💗 ได้คะแนนรวม: {score} คะแนน")
+st.info("💗 ได้คะแนนรวม: {} คะแนน".format(score))
 
 if score == 10:
     st.success("🎉 You win!")
